4b4e8a3272309a9e6733b2be198aa2fb7630e5d1
Abiola Emmanuel <sijuwolaabiola@gmail.com>
2025-11-30 23:51:11 +0100

    completed dashboard page

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -6,3 +6,101 @@ function menuOpen() {
 function closeMenu() {
   menuOverlay.classList.remove('open')
 }
+
+const buttons = document.querySelectorAll(".trends button");
+const rows = document.querySelectorAll(".crypto-row");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    buttons.forEach(b => b.classList.remove("active"));
+
+    btn.classList.add("active");
+
+    const filter = btn.dataset.filter;
+
+    if (filter === "favorites") {
+      rows.forEach(row => row.style.display = "flex");
+      resetImages();
+      return;
+    }
+
+    rows.forEach((row, index) => {
+      if (index > 3) {
+        row.style.display = "none";
+      } else {
+        row.style.display = "flex";
+      }
+    });
+
+    changeImages(filter);
+  });
+});
+
+const iconMap = {
+  top: {
+    BTC: "/images/btc.png",
+    ETH: "/images/eth.png",
+    SOL: "/images/sol.png",
+    DOGE: "/images/doge.png",
+    XRP: "/images/xrp.png",
+    USDT: "/images/usdt.png"
+  },
+
+  hot: {
+    BTC: "/images/sol.png",
+    ETH: "/images/eth.png",
+    SOL: "/images/btc.png",
+    DOGE: "/images/doge.png",
+    XRP: "/images/xrp.png",
+    USDT: "/images/usdt.png"
+  },
+
+  gainers: {
+    BTC: "/images/eth.png",
+    ETH: "/images/doge.png",
+    SOL: "/images/sol.png",
+    DOGE: "/images/btc.png",
+    XRP: "/images/xrp.png",
+    USDT: "/images/usdt.png"
+  },
+
+  new: {
+    BTC: "/images/usdt.png",
+    ETH: "/images/eth.png",
+    SOL: "/images/btc.png",
+    DOGE: "/images/doge.png",
+    XRP: "/images/xrp.png",
+    USDT: "/images/usdt.png"
+  }
+};
+
+function changeImages(filter) {
+  rows.forEach(row => {
+    const name = row.querySelector(".name").textContent.replace("/USD", "");
+    const img = row.querySelector("img.icon");
+
+    if (iconMap[filter] && iconMap[filter][name]) {
+      img.src = iconMap[filter][name];
+    }
+  });
+}
+
+function resetImages() {
+  const originalIcons = {
+    BTC: "/images/btc.png",
+    ETH: "/images/eth.png",
+    SOL: "/images/sol.png",
+    DOGE: "/images/doge.png",
+    XRP: "/images/xrp.png",
+    USDT: "/images/usdt.png"
+  };
+
+  rows.forEach(row => {
+    const name = row.querySelector(".name").textContent.replace("/USD", "");
+    const img = row.querySelector("img.icon");
+
+    if (img && originalIcons[name]) {
+      img.src = originalIcons[name];
+    }
+  });
+}
